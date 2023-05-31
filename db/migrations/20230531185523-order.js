bbbf7d10c0e730ba2f7a14a1a6bd204043893c81
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-05-31 16:48:22 -0600

    order-product

--- db/migrations/20230531185523-order.js
+++ db/migrations/20230531185523-order.js
@@ -5,10 +5,12 @@ const { CategorySchema, CATEGORY_TABLE } = require('../models/category.model');
 
 module.exports = {
   up: async (queryInterface) => {
+    await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
   },
 
   down: async (queryInterface) => {
+    await queryInterface.dropTable(PRODUCT_TABLE);
     await queryInterface.dropTable(CATEGORY_TABLE);
   },
 };
