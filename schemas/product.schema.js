ac16aa5c6f2a0897beca15eaad751af254778c4d
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-05-31 21:21:38 -0600

    Op sequelize

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -9,6 +9,8 @@ const categoryId = Joi.number().integer();
 
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
+const price_min = Joi.number().integer().min(10);
+const price_max = Joi.number().integer().min(10);
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -33,6 +35,9 @@ const getProductSchema = Joi.object({
 const queryProductSchema = Joi.object({
   limit,
   offset,
+  price,
+  price_min,
+  price_max,
 });
 
 module.exports = {
